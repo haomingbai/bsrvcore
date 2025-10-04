198f140bcf2b473637c9d72dab8fff51db9b1cdb
Haoming Bai <haomingbai@hotmail.com>
2025-10-05 00:39:31 +0800

    feat: Add a header to include all public headers for quick development.

---
+++ include/bsrvcore/bsrvcore.h
@@ -0,0 +1,29 @@
+/**
+ * @file bsrvcore.h
+ * @brief
+ * @author Haoming Bai <haomingbai@hotmail.com>
+ * @date   2025-10-04
+ *
+ * Copyright © 2025 Haoming Bai
+ * SPDX-License-Identifier: MIT
+ *
+ * @details
+ */
+
+#pragma once
+
+#ifndef BSRVCORE_BSRVCORE_H_
+#define BSRVCORE_BSRVCORE_H_
+
+#include "bsrvcore/attribute.h"
+#include "bsrvcore/context.h"
+#include "bsrvcore/http_request_aspect_handler.h"
+#include "bsrvcore/http_request_handler.h"
+#include "bsrvcore/http_request_method.h"
+#include "bsrvcore/http_route_result.h"
+#include "bsrvcore/http_server.h"
+#include "bsrvcore/http_server_task.h"
+#include "bsrvcore/logger.h"
+#include "bsrvcore/server_set_cookie.h"
+
+#endif
